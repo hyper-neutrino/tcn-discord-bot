3c900801ceff8eddf23c56572dba9b97ac919697
hyper-neutrino <alex.yj.liao@gmail.com>
2022-03-26 13:56:53 -0400

    oops

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -137,16 +137,16 @@ async function handle_poll(interaction, id, sub) {
         }
     } else if (sub == "votes") {
         return (
-            Object.keys(poll.votes || {})
-                .map(
-                    (key) =>
+            (await Promise.all(
+                Object.keys(poll.votes || {}).map(
+                    async (key) =>
                         `<@${key}> (${await tag_user(key)}): \`${
                             poll.votes[key] == -1
                                 ? "ABSTAINED"
                                 : [poll.votes[key]].flat().join(" > ")
                         }\``
                 )
-                .join("\n") || "(nobody has voted yet)"
+            ).join("\n")) || "(nobody has voted yet)"
         );
     } else if (sub == "abstain") {
         await set_poll_vote(id, user_id, -1);
