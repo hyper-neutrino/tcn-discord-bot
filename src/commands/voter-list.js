945d87721f765005f9de9c097e94f77178ff518c
hyper-neutrino <alex.yj.liao@gmail.com>
2022-06-17 12:42:01 -0400

    add voter list command

---
+++ src/commands/voter-list.js
@@ -0,0 +1,48 @@
+import { get_council, get_voters } from "../api.js";
+import { get_poll } from "../db/polls.js";
+
+export const command = {
+    name: "voter-list",
+    description: "List eligible voters and whether or not they've voted.",
+    type: "CHAT_INPUT",
+    options: [
+        {
+            name: "id",
+            description: "The poll ID.",
+            type: "STRING",
+            required: true,
+            autocomplete: true,
+        },
+    ],
+};
+
+export async function execute(interaction, { id }) {
+    const poll = await get_poll(id);
+    if (!poll) return "There is no poll by this ID.";
+
+    const eligible = poll.restrict ? await get_voters() : await get_council();
+    const voted = new Set(Object.keys(poll.votes));
+
+    const output = [];
+
+    for (const id of eligible) {
+        const user = await interaction.client.users.fetch(id);
+        output.push(`${voted.has(id) ? "✅" : "❌"} ${user.id} ${user.tag}`);
+    }
+
+    await interaction.reply({
+        files: [
+            {
+                attachment: Buffer.from(output.join("\n")),
+                name: "voters.txt",
+            },
+        ],
+    });
+}
+
+export async function autocomplete(interaction) {
+    const query = interaction.options.getFocused();
+    return (await get_polls())
+        .map((poll) => poll.id)
+        .filter((x) => x.match(query));
+}
